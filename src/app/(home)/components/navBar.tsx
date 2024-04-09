022b24c8e6c43039bf3482b000cf2fb18d8bb1e3
jaylenkubi <jaylenkubi@gmail.com>
2024-04-09 23:44:52 +0100

    navbar and typewriter effect fixed

--- src/app/(home)/components/navBar.tsx
+++ src/app/(home)/components/navBar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { SiGithub, SiLinkedin, SiX } from 'react-icons/si';
+import { SiGithub, SiLinkedin } from 'react-icons/si';
 import { cn } from '@/lib/utils';
 
 
@@ -25,9 +25,9 @@ export default function NavBar({className}: { className?: string }) {
 			<h1 className={"text-2xl font-bold underline underline-offset-8 decoration-blue-400 rotate-2"}>Jaylen Appiah-Kubi</h1>
 			<div className={"flex items-center gap-4"}>
 				{
-					socials.map((social) => {
+					socials.map((social, idx) => {
 						const Icon = social.Icon
-						return <Link href={ social.link } aria-label={ social.label }>
+						return <Link href={ social.link } key={ idx } aria-label={ social.label }>
 							<Icon className={"w-5 h-5 hover:scale-125 transition"}/>
 						</Link>
 					})
