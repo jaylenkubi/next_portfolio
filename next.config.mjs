f7cf616c101b39c3b308cab5872f634e31d0efc5
jaylenkubi <jaylenkubi@gmail.com>
2024-04-10 01:18:17 +0100

    firebase setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {images: {
-        domains: ['i.imgur.com'],
-    }};
+        domains: ['i.imgur.com']
+    },
+    output: 'export',
+    distDir: 'build'
+};
 
 export default nextConfig;
